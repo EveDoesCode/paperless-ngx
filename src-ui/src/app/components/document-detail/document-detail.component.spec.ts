37a090a5978682b5f02051f67e170e69387caa55
shamoon <4887959+shamoon@users.noreply.github.com>
2025-07-03 12:07:16 -0700

    Add error handling test for PDF editor

--- src-ui/src/app/components/document-detail/document-detail.component.spec.ts
+++ src-ui/src/app/components/document-detail/document-detail.component.spec.ts
@@ -1142,7 +1142,7 @@ describe('DocumentDetailComponent', () => {
     ).not.toBeUndefined()
   })
 
-  it('should support pdf editor', () => {
+  it('should support pdf editor, handle error', () => {
     let modal: NgbModalRef
     modalService.activeInstances.subscribe((m) => (modal = m[0]))
     initNormally()
@@ -1151,7 +1151,7 @@ describe('DocumentDetailComponent', () => {
     modal.componentInstance.documentID = doc.id
     modal.componentInstance.pages = [{ page: 1, rotate: 0, splitAfter: false }]
     modal.componentInstance.confirm()
-    const req = httpTestingController.expectOne(
+    let req = httpTestingController.expectOne(
       `${environment.apiBaseUrl}documents/bulk_edit/`
     )
     expect(req.request.body).toEqual({
@@ -1165,6 +1165,17 @@ describe('DocumentDetailComponent', () => {
       },
     })
     req.flush(true)
+
+    component.editPdf()
+    modal.componentInstance.documentID = doc.id
+    modal.componentInstance.pages = [{ page: 1, rotate: 0, splitAfter: true }]
+    modal.componentInstance.confirm()
+    const errorSpy = jest.spyOn(toastService, 'showError')
+    req = httpTestingController.expectOne(
+      `${environment.apiBaseUrl}documents/bulk_edit/`
+    )
+    req.error(new ErrorEvent('failed'))
+    expect(errorSpy).toHaveBeenCalled()
   })
 
   it('should support keyboard shortcuts', () => {
